39401d1ef4806afd12328bfbad620de4d588e710
Will Rossiter <will.rossiter@gmail.com>
2014-03-16 20:13:39 +1300

    Fix staging site not allowing any products to be tested.

--- code/product/Product.php
+++ code/product/Product.php
@@ -170,7 +170,7 @@ class Product extends Page implements Buyable {
 	public function canPurchase($member = null, $quantity = 1) {
 		$global = self::config()->global_allow_purchase;
 
-		if(!$global || !$this->AllowPurchase || !$this->isPublished()) {
+		if(!$global || !$this->AllowPurchase) {
 			return false;
 		}
 
